b49051c650c339a2c5a7cc4805230e0964eab4fb
K.Gluschenko <k.gluschenko@vis-design.com>
2017-03-30 13:25:36 +0200

    more api coverage

--- README.md
+++ README.md
@@ -32,15 +32,82 @@ Methods example
     $result  = $client->getVersion();
 ```
 
+Send identical email to every recipient with prepared template as %TEMPLATE.variable_name%
 ```php
-    $letterTemplate = 'test';
-    $recipient = ['k.gluschenko@vis-design.com'];
-    $params = ["name" => "k.gluschenko", "message" => "test_letter"];
+    $letterTemplate = 'email_send';
+    $recipient = ['email@email.com'];
+    $params    = ['name' => 'k.glushchenko', 'message' => 'test_letter'];
 
     $result  = $client->sendPreparedMessage($letterTemplate, $recipient, $params);
 ```
 
+Send identical sms to every recipient with prepared template  Params in template are defined as %TEMPLATE.variable_name%
 ```php
-    $id = 'some-id';
-    $result = $client->getInstantEmailStatus($id);
+    $letterTemplate = 'sms_send';
+    $recipient = ['+38(000)-000-00-00'];
+    $params    = ['name' => 'k.glushchenko', 'message' => 'test_letter'];
+
+    $result  = $client->sendPreparedMessage($letterTemplate, $recipient, $params, false);
 ```
+
+Send parametrized email for every recipient with prepared template. Params in template are defined as $!data.get('variable_name')
+```php
+
+    $letterTemplate = 'email_smartsend';
+    $recipient = ['email1@email.com', 'email2@email.com'];
+    $params    = [
+        ['name' => 'name_for_email1', 'message' => 'message_for_email1'],
+        ['name' => 'name_for_email2', 'message' => 'message_for_email2']
+    ];
+
+    $result  = $client->sendExtendedPreparedMessage($letterTemplate, $recipient, $params);
+```
+
+Send parametrized sms for every recipient with prepared template. Params in template are defined as $!data.get('variable_name')
+```php
+
+    $letterTemplate = 'sms_smartsend';
+    $recipient = ['+38(000)-000-000-00', '+38(000)-000-00-01'];
+    $params    = [
+        ['name' => 'name_for_00', 'message' => 'message_for_00'],
+        ['name' => 'name_for_01', 'message' => 'message_for_01']
+    ];
+
+    $result  = $client->sendExtendedPreparedMessage($letterTemplate, $recipient, $params, false);
+```
+
+Check message status by message id
+```php
+    $result = $client->getInstantMessageStatus($id);
+```
+
+Send instant email
+```php
+    $from       = '"organization" <your@account.com>';
+    $subject    = 'subject';
+    $htmlText   = '<html><body><h1>test!</h1></body></html>';
+    $emails     = ['email@email.com'];
+
+    $result  = $client->sendEmail($from, $subject, $htmlText,$emails);
+```
+
+Check email status by message hash
+```php
+    $result = $client->getInstantEmailStatus($hash);
+```
+
+Send instant email
+
+```php
+     $from   = 'your_sms_sender';
+     $text   = 'test';
+     $phones = ["+38(000)-000-00-00"];
+
+     $result  = $client->sendSMS($from, $text, $phones);
+```
+
+Check sms status by message hash
+```php
+    $result = $client->getInstantSmsStatus($hash);
+```
+
--- src/Models/eSputnikClient.php
+++ src/Models/eSputnikClient.php
@@ -4,8 +4,9 @@ use Illuminate\Support\Facades\Config;
 use Vis\CurlClient\CurlClient;
 
 /**
- * API coverage 3/43 methods
- *@link Methods names are identical to WADL file https://esputnik.com/api/application.wadl
+ * @link Api docs https://esputnik.com/api/
+ * @link Api integration docs https://esputnik.com/support/integraciya-s-api
+ * @link Methods names are identical to WADL https://esputnik.com/api/application.wadl
  */
 
 class eSputnikClient
@@ -15,7 +16,7 @@ class eSputnikClient
 
     public function __construct()
     {
-        $this->apiUrl = "https://esputnik.com/api/v1/";
+        $this->apiUrl = 'https://esputnik.com/api/v1/';
 
         $this->curl = New CurlClient();
         $this->curl->setRequestCredentials($this->getConfigValue('login'), $this->getConfigValue('password'))
@@ -50,7 +51,7 @@ class eSputnikClient
     private function getFullUrl($segment, $replacement = null)
     {
         if ($replacement) {
-            $segment = str_replace("{id}", $replacement, $segment);
+            $segment = str_replace('{id}', $replacement, $segment);
         }
 
         return $this->getApiUrl() . $segment;
@@ -70,21 +71,42 @@ class eSputnikClient
 
     /**
      * should this method be a method ?
-     * @param array $array
-     * @return array
+     * @param array $params
+     * @return array $preparedParams
      */
-    private function prepareParams(array $array)
+    private function prepareParams(array $params)
     {
-        $params = [];
+        $preparedParams = [];
 
-        foreach($array as $key => $value){
-            $params[] = [
+        foreach($params as $key => $value){
+            $preparedParams[] = [
                 'key'   =>   $key,
-                'value' =>   $value ?: ""
+                'value' =>   $value ?: ''
             ];
         }
 
-        return $params;
+        return $preparedParams;
+    }
+
+    /**
+     * should this method be a method ?
+     * @param array $recipients
+     * @param array $params
+     * @return array $preparedParams
+     */
+    private function prepareExtendedParams(array $recipients, array $params)
+    {
+        $count = count($recipients)-1;
+        $preparedParams = [];
+
+        for($i = 0; $i <= $count; $i++){
+            $preparedParams[$i] =[
+                'email'     => $recipients[$i],
+                'jsonParam' => json_encode($params[$i])
+            ] ;
+        }
+
+        return $preparedParams;
     }
 
     /**
@@ -93,28 +115,29 @@ class eSputnikClient
      */
     public function getVersion()
     {
-        $url = $this->getFullUrl("version");
+        $url = $this->getFullUrl('version');
 
         return $this->useCurl('GET', $url);
     }
 
     /**
      * @link https://esputnik.com/api/methods.html#/v1/message/{id}/send-POST
-     * @param $template
+     * @param string $templateName
      * @param array $recipients
      * @param boolean $email
-     * @param $params
+     * @param array $params
      * @return mixed
      */
-    public function sendPreparedMessage($template, array $recipients = [], $params = [], $email = true )
+    public function sendPreparedMessage($templateName, array $recipients = [], array $params = [], $email = true)
     {
-        $template = $this->getConfigValue('prepared_message_templates')[$template] ?: 0;
+        $templates = $this->getConfigValue('prepared_message_templates');
+        $template  = isset($templates[$templateName]) ? $templates[$templateName] : 0;
 
         if(!$template  || !count($recipients)){
             return false;
         }
 
-        $url = $this->getFullUrl("message/{id}/send", $template);
+        $url = $this->getFullUrl('message/{id}/send', $template);
 
         $preparedParams = [
             'email'      => $email,
@@ -126,14 +149,122 @@ class eSputnikClient
     }
 
     /**
+     * @link https://esputnik.com/api/methods.html#/v1/message/{id}/smartsend-POST
+     * @param string $templateName
+     * @param array $recipients
+     * @param boolean $email
+     * @param array $params
+     * @return mixed
+     */
+    public function sendExtendedPreparedMessage($templateName, array $recipients = [], array $params = [], $email = true)
+    {
+        $templates = $this->getConfigValue('prepared_message_templates');
+        $template  = isset($templates[$templateName]) ? $templates[$templateName] : 0;
+
+        if(!$template  || !count($recipients) || (count($recipients) != count($params))){
+            return false;
+        }
+
+        $url = $this->getFullUrl('message/{id}/smartsend', $template);
+
+        $preparedParams = [
+            'email'      => $email,
+            'recipients' => $this->prepareExtendedParams($recipients,$params),
+        ];
+
+        return $this->useCurl('POST', $url, [], $preparedParams);
+    }
+
+    /**
+     * @link ????
+     * @param int $id
+     * @return mixed
+     */
+
+    public function getInstantMessageStatus($id)
+    {
+        $url = $this->getFullUrl('message/status');
+        $preparedParams = ['ids' => $id];
+
+        return $this->useCurl('GET', $url, $preparedParams);
+    }
+
+    /**
+     * @link https://esputnik.com/api/example_v1_message_email_POST.html
+     * @param string $from
+     * @param string $subject
+     * @param string $htmltext
+     * @param string $plaintext
+     * @param array $emails
+     * @return mixed
+     */
+    public function sendEmail($from, $subject, $htmltext, $emails = [], $plaintext = '')
+    {
+        if(!count($emails)){
+            return false;
+        }
+
+        $url = $this->getFullUrl('message/email');
+
+        $preparedParams = [
+            'from'       => $from,
+            'subject'    => $subject,
+            'htmlText'   => $htmltext,
+            'emails'     => $emails,
+            'plaintText' => $plaintext,
+        ];
+
+        return $this->useCurl('POST', $url, [], $preparedParams);
+    }
+
+    /**
      * @link https://esputnik.com/api/methods.html#/v1/message/email/status-GET
-     * @param $id
+     * @param string $id
      * @return mixed
      */
 
     public function getInstantEmailStatus($id)
     {
-        $url = $this->getFullUrl("message/email/status");
+        $url = $this->getFullUrl('message/email/status');
+        $preparedParams = ['ids' => $id];
+
+        return $this->useCurl('GET', $url, $preparedParams);
+    }
+
+
+    /**
+     * @link https://esputnik.com/api/example_v1_message_sms_POST.html
+     * @param string $from
+     * @param string $text
+     * @param array $phones
+     * @return mixed
+     */
+    public function sendSMS($from, $text, $phones = [])
+    {
+        if(!count($phones)){
+            return false;
+        }
+
+        $url = $this->getFullUrl('message/sms');
+
+        $preparedParams = [
+            'from'         => $from,
+            'text'         => $text,
+            'phoneNumbers' => $phones,
+        ];
+
+        return $this->useCurl('POST', $url, [], $preparedParams);
+    }
+
+    /**
+     * @link https://esputnik.com/api/methods.html#/v1/message/sms/status-GET
+     * @param string $id
+     * @return mixed
+     */
+
+    public function getInstantSmsStatus($id)
+    {
+        $url = $this->getFullUrl('message/sms/status');
         $preparedParams = ['ids' => $id];
 
         return $this->useCurl('GET', $url, $preparedParams);
